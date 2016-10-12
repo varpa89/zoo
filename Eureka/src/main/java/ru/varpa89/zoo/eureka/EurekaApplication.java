33af0feeaa0284c676330b17bd63907408da6a51
pavel <varpa89@gmail.com>
2016-10-12 21:52:46 +0300

    zoo init commit

---
+++ Eureka/src/main/java/ru/varpa89/zoo/eureka/EurekaApplication.java
@@ -0,0 +1,23 @@
+package ru.varpa89.zoo.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author varpa89
+ * @since 12.10.2016
+ */
+@SpringBootApplication
+@EnableEurekaServer
+public class EurekaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaApplication.class, args);
+    }
+}
